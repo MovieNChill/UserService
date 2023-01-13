0e3b5f8bfc0257caec8f66acb8ed6eae7abaf7de
tixy74 <timlab74@gmail.com>
2023-01-13 10:11:56 +0100

    password message update

--- src/main/java/com/movienchill/userservice/exception/PasswordInvalidException.java
+++ src/main/java/com/movienchill/userservice/exception/PasswordInvalidException.java
@@ -6,7 +6,7 @@ public class PasswordInvalidException extends CustomException {
     public String code;
 
     public PasswordInvalidException() {
-        super("The password should contain at least one digit, one lower case, one upper case, one special character and no whitespace",
+        super("The password is not strong enough",
                 "invalid_password");
     }
 }
